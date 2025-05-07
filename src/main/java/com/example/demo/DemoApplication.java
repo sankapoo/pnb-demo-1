56de8518d617bfe5e2dfa220c4146683413128d9
Sandeep Kapoor <sankapoo@redhat.com>
2025-05-08 03:59:11 +0530

    updated

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,8 +11,16 @@ public class DemoApplication {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
     }
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello() {
-      return String.format("Welcome, to the Red Hat Openshift Pipelines");
+      return String.format("What is Red Hat OpenShift?
+   Red Hat OpenShift is a Kubernetes-based enterprise container platform that helps developers and IT teams build, deploy,
+ and manage applications at scale. It provides a secure, automated, and self-service platform for running containerized workloads,
+  with features like built-in CI/CD, monitoring, multi-tenancy, and developer tools.
+
+What are OpenShift Pipelines?
+OpenShift Pipelines is Red Hat’s CI/CD framework based on Tekton, designed to build, test, and deploy applications on OpenShift.
+It enables you to define CI/CD workflows as Kubernetes-native objects, using custom Tasks and Pipelines.
+It supports fully automated, cloud-native, and scalable DevOps workflows without needing a central CI server.");
     }
 }
