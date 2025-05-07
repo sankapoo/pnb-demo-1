fd03033df64862d53439198dd47aeceffef2fbee
Sandeep Kapoor <sankapoo@redhat.com>
2025-05-08 04:08:58 +0530

    fix

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,16 +11,17 @@ public class DemoApplication {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
     }
+
     @GetMapping("/")
     public String hello() {
-      return String.format("What is Red Hat OpenShift?
-   Red Hat OpenShift is a Kubernetes-based enterprise container platform that helps developers and IT teams build, deploy,
- and manage applications at scale. It provides a secure, automated, and self-service platform for running containerized workloads,
-  with features like built-in CI/CD, monitoring, multi-tenancy, and developer tools.
-
-What are OpenShift Pipelines?
-OpenShift Pipelines is Red Hat’s CI/CD framework based on Tekton, designed to build, test, and deploy applications on OpenShift.
-It enables you to define CI/CD workflows as Kubernetes-native objects, using custom Tasks and Pipelines.
-It supports fully automated, cloud-native, and scalable DevOps workflows without needing a central CI server.");
+      return "What is Red Hat OpenShift?\n" +
+             "Red Hat OpenShift is a Kubernetes-based enterprise container platform that helps developers and IT teams build, deploy,\n" +
+             "and manage applications at scale. It provides a secure, automated, and self-service platform for running containerized workloads,\n" +
+             "with features like built-in CI/CD, monitoring, multi-tenancy, and developer tools.\n\n" +
+             "What are OpenShift Pipelines?\n" +
+             "OpenShift Pipelines is Red Hat’s CI/CD framework based on Tekton, designed to build, test, and deploy applications on OpenShift.\n" +
+             "It enables you to define CI/CD workflows as Kubernetes-native objects, using custom Tasks and Pipelines.\n" +
+             "It supports fully automated, cloud-native, and scalable DevOps workflows without needing a central CI server.";
     }
+
 }
